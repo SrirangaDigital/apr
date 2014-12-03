539de63cc46afc5cb0aa04af95527b501b1456a5
suresh07 <soori_rozee@yahoo.com>
2014-12-03 16:10:48 +0530

    Commit No. 18 Updated December issue

--- php/currentissue.php
+++ php/currentissue.php
@@ -8,7 +8,7 @@
 	$currentmonth=$row['month'];
 	
 	echo "<div class=\"holder\">";
-	echo "<div class=\"holder_title\"><img src=\"../php/images/theme.png\" alt=\"Theme Image\"/><a href=\"../Volumes/".$currentyear."_".$currentmonth.".pdf#page=1\" target=\"_blank\">".$month[$currentmonth-1]." ".$currentyear."</a></div>";
+	echo "<div class=\"holder_title\"><img src=\"../php/images/theme.png\" alt=\"Theme Image\"/>&nbsp;<a href=\"../Volumes/".$currentyear."_".$currentmonth.".pdf#page=1\" target=\"_blank\">".$month[$currentmonth-1]." ".$currentyear."</a></div>";
 	echo "<div class=\"holder_area\">";
 	$result=mysql_query("select * from article where volume = $volume and issue= $issue ");
 	$num_rows=	mysql_num_rows($result);
