9dc02e7b5d038477dd6a0407791c1854bd7394cf
suresh07 <soori_rozee@yahoo.com>
2017-07-31 11:36:13 +0530

    August issue updated

--- php/connect_db.php
+++ php/connect_db.php
@@ -3,9 +3,13 @@
 	$usr="root";
 	$pwd="mysql";
 	$db="apr";
-	$volume='34';
-	$issue='10';
 	
 	$mysqli = new mysqli("$host","$usr","$pwd", "$db");
 	mysqli_set_charset($mysqli,"utf8");
+	
+	$query = "SELECT  volume, issue FROM article WHERE volume = (SELECT volume FROM article ORDER BY volume DESC LIMIT 1) ORDER BY issue DESC LIMIT 1;";
+	$result = $mysqli->query($query);
+	$row = $result->fetch_assoc();
+	$volume = $row['volume'];
+	$issue = $row['issue'];
 ?>
